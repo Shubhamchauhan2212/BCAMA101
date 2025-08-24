430ead050cba20ff4a16e4713fa94f8428022866
Shubham Chauhan <shubhambchauhan2212@gmail.com>
2025-08-24 19:17:29 +0530

    Add files via upload

---
+++ programe24.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] ={1, 2, 3, 4, 5};
+    int b[] ={10, 20, 30, 40, 50};
+    int n = 5;
+    printf("addition of array: ");
+     for (int i = 0; i < n; i++)
+     {
+        int c = a[i] * b[i];
+        printf("%d ",c);
+     }
+     
+}
