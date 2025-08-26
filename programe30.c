dbdf8da45a8584f11e3810a0f81e8597f7877fe5
Shubham Chauhan <shubhambchauhan2212@gmail.com>
2025-08-26 22:37:36 +0530

    Add files via upload

---
+++ programe30.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    char str1[100],str2[100],merged[200];
+
+    printf("Enter the first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+
+    printf("Enter the Second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = '\0';
+
+     strcpy(merged, str1);
+     strcat(merged, str2);
+
+     printf("Merged string: %s", merged);
+    return 0;
+}
